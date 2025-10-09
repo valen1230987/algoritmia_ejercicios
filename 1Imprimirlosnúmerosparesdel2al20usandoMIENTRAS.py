6430c58958ba6ef53a7f984924a7ab0496ab9d90
Valentina <valentinacalderonlopez19@gmail.com>
2025-10-09 01:25:55 -0500

    Subir cambios recientes

---
+++ 1Imprimirlosnúmerosparesdel2al20usandoMIENTRAS.py
@@ -0,0 +1,15 @@
+#Nombre: Programa para imprimir los números pares del 2 al 20 usando MIENTRAS.
+
+#Entradas:
+#Nombre: Valentina 
+#Apellidos: Calderon Lopez
+
+#Salidas: 
+# num: se muestran los números pares del 2 al 20
+
+#Proceso: Se imprime la secuencia de números pares desde el 2 hasta el 20 utilizando MIENTRAS.
+
+num = 2
+while num <= 20:
+    print(num)
+    num = num + 2
